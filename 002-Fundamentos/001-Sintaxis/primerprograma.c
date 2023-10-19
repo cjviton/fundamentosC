e163b3a010e37c25b7a756a9d142730e9e59dd88
CarlosJV <cjimenezv334@alumnos.imf.com>
2023-10-19 22:29:00 +0200

    Final
    pequeños cambios

--- 002-Fundamentos/001-Sintaxis/primerprograma.c
+++ 002-Fundamentos/001-Sintaxis/primerprograma.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
+#include <stdio.h>                   //con esto incluyo la libreria standar io, entradas  y salidas .h indica que es una cabecera de C
 
-int main(int argc,char *argv[]){
+int main(int argc,char *argv[]){     //Tengo que declarar un punto de entrada de ejecución se tiene que llamar obligatoriamente main
+                                     //seguido de char y un array de argumentos
     
-    printf("Hola mundo\n");
+    printf("Hola Carlos Jimenez \n");
     
-    return 0;
+    return 0;                        //como indiqué en la declaración tiene que devolver un int, un número entero
 }
 
